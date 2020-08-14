a519878cf17aede48749eeda6e9286bfcba2a22d
desperatecat <376198935@qq.com>
2020-08-14 23:17:24 +0800

    add snackbar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Tooltip from './ninja-ui/tooltip';
 import Dropdown from './ninja-ui/dropdown';
+import Tabs from './ninja-ui/tabs';
+import Snackbar from './ninja-ui/snackbar';
 
 // create tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
@@ -11,4 +13,17 @@ const dropdowns = document.querySelectorAll('.dropdown');
 dropdowns.forEach(dropdown => {
   const instance = new Dropdown(dropdown);
   instance.init();
+});
+
+// create tabs
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
+
+// create snackbar
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+  snackbar.show('you clicked me :)');
 });
